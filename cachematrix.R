4c017a533252d31acd9f8eae5fc63303246172f6
lovsn <stephen.lovelady@unilever.com>
2015-02-20 13:37:23 +0000

    Solution for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Here are two functions, makeCacheMatrix and cacheSolve, which work together
+## to invert a square matrix by one of two methods. The first is to check 
+## whether the matrix has been inverted previously, and, if so, return the 
+## inverse from a cache. The second is to invert the matrix using the solve() 
+## function, and store the inverse to the cache should it be required in future
 
-## Write a short comment describing this function
+## makeCacheMatrix wraps four separate functions around a matrix x, and is used
+## to store the matrix, and also its inverse, in a cache for future use
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) {     
+        InV <- NULL
+        set <- function(y) {
+                x <<- y
+                InV <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) InV <<- inverse
+        getinverse <- function() InV
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is used to invert the matrix stored in makeCacheMatrix via one of
+## the two methods described in the introduction. It will make use of the
+## property that the only way the matrix can have its inverse stored in the
+## cache is if it has been calculated previously
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {        
+        InV <- x$getinverse()
+        if(!is.null(InV)) {
+                message("getting cached data")
+                return(InV)
+        }
+        data <- x$get()
+        InV <- solve(data, ...)
+        x$setinverse(InV)
+        InV
+        
 }
